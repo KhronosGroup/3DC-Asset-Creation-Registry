1fa84f8fe6a6360133368390413423333d14a369
Jon Leech <oddhack@sonic.net>
2020-10-20 18:04:13 -0700

    Create registry and publish version 1.0.0 of Asset Creation Guidelines.

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# 3DC-Asset-Creation-Registry
+# 3D Commerce Asset Creation Models Registry
+
+The 3DC-Asset-Creation-Registry repository contains published Khronos 3D
+Commerce Working Group Specifications for Asset Creation Models.
+
+It is also used as a backing store for the web view of the registry at
+https://www.khronos.org/registry/3dc-assetcreationmodels ; commits to the
+main branch of this repository will be reflected there.
+
+Interesting files in this repository include:
+
+* index.php - Toplevel index page for the web view. This relies on PHP
+  include files found elsewhere on www.khronos.org and so is not very useful
+  in isolation.
+* specs/ - Specification documents.
---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+$static_title = 'Khronos 3D Commerce Asset Creation Models Specification Registry';
+
+include_once("../../assets/static_pages/khr_page_top.php");
+?>
+
+<p> The Khronos 3D Commerce Asset Creation Models Specification registry
+    contains specifications created by the 3D Commerce Working Group. </p>
+
+<hr />
+
+<h2>Current Khronos 3D Commerce Asset Creation Models Specification</h2>
+
+<ul>
+  <li> <b>Real-time Asset Creation Guidelines</b> version 1.0.0, Last Updated: October 20, 2020 <br />
+    <ul>
+      <li> <a href="specs/1.0/AssetCreationGuidelines.pdf">Specification as PDF</a> </li>
+    </ul>
+  </li>
+</ul>
+
+<hr />
+
+<h2> Providing Feedback on the Registry </h6>
+
+<p> Khronos welcomes comments and bug reports. To provide feedback on the
+    Registry itself (such as reporting missing content, bad links, etc.),
+    file an issue in the <a
+    href="https://github.com/KhronosGroup/3DC-Asset-Creation-Registry/issues">
+    3DC-Asset-Creation-Registry </a> Github project. </p>
+
+<p> For feedback on the Specifications, file an issue in the <a
+    href="https://github.com/KhronosGroup/https://github.com/KhronosGroup/3DC-Asset-Creation">
+    3D Commerce Asset Creation</a> Github project. </p>
+
+<?php include_once("../../assets/static_pages/khr_page_bottom.php"); ?>
+</body>
+</html>
